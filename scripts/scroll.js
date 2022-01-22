f54b2340c6eb09551e037e0c87a39c90d1ec7fcd
arqu33n <soondawn@yandex.ru>
2022-01-22 19:29:43 +0300

    complete tabs

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,33 +1,40 @@
-const links = document.querySelectorAll(".header-menu__item a");
-seamless.polyfill();
-links.forEach((element) => {
-  element.addEventListener("click", (event) => {
-    event.preventDefault();
-    const id = element.getAttribute("href").substring(1);
-    const section = document.getElementById(id);
-    if (section) {
-      seamless.elementScrollIntoView(section, {
-        behavior: "smooth",
-        block: "start",
-      });
-    } else {
-      seamless.elementScrollIntoView(
-        document.querySelector("#characteristics"),
-        {
+const scrollFunc = () => {
+  const links = document.querySelectorAll(".header-menu__item a");
+  const cardLink = document.querySelector(
+    ".card-details__link-characteristics"
+  );
+
+  const newArray = [...links, cardLink];
+  seamless.polyfill();
+  newArray.forEach((element) => {
+    element.addEventListener("click", (event) => {
+      event.preventDefault();
+      const id = element.getAttribute("href").substring(1);
+      const section = document.getElementById(id);
+      if (section) {
+        seamless.elementScrollIntoView(section, {
           behavior: "smooth",
-          block: "center",
-          inline: "center",
-        }
-      );
-    }
+          block: "start",
+        });
+      } else {
+        seamless.elementScrollIntoView(
+          document.querySelector("#characteristics"),
+          {
+            behavior: "smooth",
+            block: "center",
+            inline: "center",
+          }
+        );
+      }
+    });
   });
-});
-const cardLink = document.querySelector(".card-details__link-characteristics");
-cardLink.addEventListener("click", (event) => {
-  event.preventDefault();
-  const characteristicsSection = document.querySelector(cardLink.hash);
-  seamless.elementScrollIntoView(characteristicsSection, {
-    behavior: "smooth",
-    block: "start",
-  });
-});
+  // cardLink.addEventListener("click", (event) => {
+  //   event.preventDefault();
+  //   const characteristicsSection = document.querySelector(cardLink.hash);
+  //   seamless.elementScrollIntoView(characteristicsSection, {
+  //     behavior: "smooth",
+  //     block: "start",
+  //   });
+  // });
+};
+scrollFunc();
